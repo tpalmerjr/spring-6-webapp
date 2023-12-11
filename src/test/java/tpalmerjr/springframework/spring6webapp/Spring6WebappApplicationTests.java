4aff7aff7e3f905a686e934648d9accecab33223
Terry Palmer <tpalmerjr@gmail.com>
2023-12-11 15:51:57 -0600

    initial commit

---
+++ src/test/java/tpalmerjr/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package tpalmerjr.springframework.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
