4d06225651a9aae5fc72053643e39fb5d37a66bd
Terry Palmer <tpalmerjr@gmail.com>
2023-12-11 19:38:11 -0600

    3-jpa-repositories

---
+++ src/main/java/tpalmerjr/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package tpalmerjr.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import tpalmerjr.springframework.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
